b7ef9162141aa4cf5a0b9472d963fae39ad99568
dedot-3799 <139524254+dedot-3799@users.noreply.github.com>
2024-06-10 00:37:35 +0900

    Update main.js

--- prime/v10/main.js
+++ prime/v10/main.js
@@ -675,9 +675,9 @@ async function run_PRIME_single(c) {
                 } else {
                     var res = lang_data[lang]["isNotPrime"]
                 }
-                result.innerHTML = `${res}`
-                resu1t.innerHTML = `${String(document.getElementById("res").value).length}桁の自然数${document.getElementById("res").value.match(/.{3}/g).join("\n")}は`
                 var edt = performance.now()
+                result.innerHTML = `${res}`
+                resu1t.innerHTML = `${String(document.getElementById("res").value).length}桁の自然数${String(document.getElementById("res").value).match(/.{3}/g).join("\n")}は`
                 var ert = Math.floor((edt - t) * (10 ** 5)) / (10 ** 5);
                 ti.innerHTML = `計算にかかった時間は${ert}msでした。`
                 break;
